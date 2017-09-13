ed87a5e5159d70e896f4eef1283dc3b22d01bf14
Shengquan Ni <shengqun@uci.edu>
2017-09-13 19:54:42 +0800

    add new solutions

---
+++ Poor Pigs.cpp
@@ -0,0 +1,1 @@
+class Solution { public:     int poorPigs(int buckets, int minutesToDie, int minutesToTest) {         int k=minutesToTest/minutesToDie+1;         int res=0;         while(pow(k,res)<buckets)++res;         return res;     } };
